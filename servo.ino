832a55b6fbb620a391cdd830a11e6aaed20abd61
zzeonii <129237950+zzeonii@users.noreply.github.com>
2023-04-11 17:38:48 +0900

    응용하기

--- servo.ino
+++ servo.ino
@@ -28,26 +28,33 @@ void setup() {
 
 void loop() {
   int buttonInput = digitalRead(buttonPin);
-  if (buttonInput == 1) servo.write(150);
-  else servo.write(30);
-  // if (Serial.available()) {
-  //   char userInput = Serial.read();
+  if (buttonInput == 1) {
+    for (int angle = 0; angle <= 180; angle++) {
+      servo.write(angle);
+      delay(30);
+    }
+    // servo.write(150);
+  }else servo.write(0);
+    // if (Serial.available()) {
+    //   char userInput = Serial.read();
 
-  //   switch (userInput) {
-  //     case '1':
-  //       servo.write(30);
-  //       delay(1000);
-  //       break;
-  //     case '2':
-  //       servo.write(90);
-  //       delay(1000);
-  //       break;
-  //     case '3':
-  //       servo.write(150);
-  //       delay(1000);
-  //       break;
-  //     default:
-  //       break;
-  //   }
-  // }
-}
+    //   switch (userInput) {
+    //     case '1':
+    //       servo.write(30);
+    //       delay(1000);
+    //       break;
+    //     case '2':
+    //       servo.write(90);
+    //       delay(1000);
+    //       break;
+    //     case '3':
+    //       servo.write(150);
+    //       delay(1000);
+    //       break;
+    //     default:
+    //       break;
+    //   }
+    // }
+  }
+  
+
