c56d17f1daab6caeda272dbe057f3215043b6c7d
zzeonii <129237950+zzeonii@users.noreply.github.com>
2023-04-11 15:26:44 +0900

    add angle
    서보 모터 0~180도 조절

--- servo.ino
+++ servo.ino
@@ -9,11 +9,16 @@ void setup() {
 
   delay(1000);
 
-  for (int cnt = 0; cnt <= 2; cnt++) {
-    servo.write(0);
-    delay(1000);
-    servo.write(90);
-    delay(1000);
+  // for (int cnt = 0; cnt <= 2; cnt++) {
+  //   servo.write(0);
+  //   delay(1000);
+  //   servo.write(90);
+  //   delay(1000);
+  // }
+
+  for (int angle = 0; angle <= 180; angle++) {
+    servo.write(angle);
+    delay(30);
   }
 
   servo.detach();
