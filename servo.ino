9826126595c6f1af5c244c783abee5b321b5f9bf
zzeonii <129237950+zzeonii@users.noreply.github.com>
2023-04-11 15:25:32 +0900

    서보모터 각도 조절
    서보모터 각도 조절해보기

---
+++ servo.ino
@@ -0,0 +1,23 @@
+#include <Servo.h>
+
+const int SERVO = 10;
+Servo servo;
+
+void setup() {
+  servo.attach(SERVO);
+  servo.write(0);
+
+  delay(1000);
+
+  for (int cnt = 0; cnt <= 2; cnt++) {
+    servo.write(0);
+    delay(1000);
+    servo.write(90);
+    delay(1000);
+  }
+
+  servo.detach();
+}
+
+void loop() {
+}
